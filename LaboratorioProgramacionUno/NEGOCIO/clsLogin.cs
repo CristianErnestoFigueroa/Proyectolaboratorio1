ec934724ea816c41dc4a0460ae71ed845e3e5c1a
ErickD70 <78987375+ErickD70@users.noreply.github.com>
2021-03-01 21:17:04 -0600

    Proyectolaboratorio1

---
+++ LaboratorioProgramacionUno/NEGOCIO/clsLogin.cs
@@ -0,0 +1,47 @@
+﻿using LaboratorioProgramacionUno.DAO;
+using LaboratorioProgramacionUno.DOMINIO;
+using LaboratorioProgramacionUno.VISTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgramacionUno.NEGOCIO
+{
+    class clsLogin
+    {
+        listadoDeUsuarios list = new listadoDeUsuarios();
+        listadoDeAdministradores listAdmins = new listadoDeAdministradores();
+
+        public int Users(Login log) {
+
+            int variableAcceso = 0;
+            if (log.Estado == 0)
+            {
+                for (int i = 0; i < list.nombre.Length; i++)
+                {
+                    if (log.Usuario.Equals(list.nombre[i]) && log.Password.Equals(list.password[i]))
+                    {
+                        variableAcceso = 1;
+                    }
+                }
+                return variableAcceso;
+            }
+            else if (log.Estado == 1)
+            {
+                for (int i = 0; i < listAdmins.nombre.Length; i++)
+                {
+                    if (log.Usuario.Equals(listAdmins.nombre[i]) && log.Password.Equals(listAdmins.password[i]))
+                    {
+                        variableAcceso = 1;
+                    }
+                }
+                return variableAcceso;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+}
